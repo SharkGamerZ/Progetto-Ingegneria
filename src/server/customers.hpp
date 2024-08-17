79550e76c6a93416d84fe846569fed4d9bf6cd1d
SharkGamerZ <marcellettimatteo02@gmail.com>
2024-08-17 12:14:30 +0200

    Struct customers

--- src/server/customers.hpp
+++ src/server/customers.hpp
@@ -1,1 +1,25 @@
 #include "utils.hpp"
+
+struct Customer {
+    string nome;
+    string cognome;
+    string mail;
+    string password;
+
+    vector<Articolo> carrello;
+    vector<Ordine> ordini_passati;
+};
+
+struct IndirizzoSpedizione {
+    int ID;
+    string citta;
+    string via;
+    string civico;
+};
+
+struct Pagamento {
+    int ID;
+    string numero_carta;
+    string scadenza;
+};
+
