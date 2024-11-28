95fa23b207560f0fd9d51c504267924ef54ae818
Sharp-p <thomassharp2003@gmail.com>
2024-11-28 18:23:34 +0100

    getData() e fetchFromDatabase() completate

--- src/dbutils/rdutils.hpp
+++ src/dbutils/rdutils.hpp
@@ -6,6 +6,7 @@
 #include "../server/utils.hpp"
 #include "pgutils.hpp"
 
+/** @brief Class that creates the redis cache and operates on it */
 class RedisCache{
 public:
     /** @brief Connects to redis. */
@@ -39,16 +40,32 @@ private:
     redisContext* context;
 };
 
+/** @brief Class that is used as interface with RedisCache */
 class DataService{
 public:
     DataService(RedisCache& cache);
 
-    string getData(const string& key);
+    /** @brief Given the table and ID of a tuple in the db, returns the values in a string.
+     * 
+     * The function before checks if the searched data are in the redis cache, if it is it returns it, otherwise it performs a select on the DB, loads the data in the cache and then returns it. 
+     * @param table The table in which is going to be searched the ID
+     * @param ID The ID of the search tuple
+    */
+    string getData(const string& table, const string& ID);
+
+
+    //void setData(const string& table, const vector<any>& values);
 
 private:
     RedisCache& cache;
 
-    //da vedere fetchFromDatabase se va tenuto
+    /** @brief Performs a select on the DB
+     * 
+     * The function is a private function called from the function getData in the case the pair table-ID is not found as a key in the redis cache.
+     * @param table The table in which is going to be searched the ID
+     * @param ID The ID of the search tuple
+     */
+    string fetchFromDatabase(const string& table, const string& ID);
 };
 
 #endif
