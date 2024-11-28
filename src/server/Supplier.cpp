c0ae8a50fe35df38edf6a0176dc645f912b9e0fa
Sharp-p <thomassharp2003@gmail.com>
2024-11-28 17:19:48 +0100

    Supplier.hpp finito e impostate funzioni in Supplier.cpp

--- src/server/Supplier.cpp
+++ src/server/Supplier.cpp
@@ -1,18 +1,27 @@
 #include "Supplier.hpp"
 
+Supplier::Supplier(string P_IVA, string email, string pw, vector<int> ID_wh, int ID, string CF, string name, string surname) {
+        this->P_IVA = P_IVA;
+        this->email = email;
+        this->pw = pw;
+        this->ID_wh = ID_wh;
+        this->CF = CF;
+        this->name = name;
+        this->surname = surname;
+    }
 
-void addProduct(Product a) {
+void Supplier::addProduct(Product a, int q) {
     bool exist = false; // TODO query per controllare se l'articolo è già presente
     if (!exist) {
         // TODO query per aggiungere l'articolo
     }
 }
 
-void setDiscontinuedProduct(Product a) {
+void Supplier::setDiscontinuedProduct(Product a) {
     // TODO
 }
 
-vector<Product> getPastOrders(Produttore p) {
+vector<Product> Supplier::getPastOrders() {
     // TODO
     return vector<Product>();
 }
--- src/server/Supplier.hpp
+++ src/server/Supplier.hpp
@@ -14,22 +14,14 @@ public:
     string pw;
     vector<int> ID_wh;
     
-    Supplier(string P_IVA, string email, string pw, vector<int> ID_wh, int ID, string CF, string name, string surname) {
-        this->P_IVA = P_IVA;
-        this->email = email;
-        this->pw = pw;
-        this->ID_wh = ID_wh;
-        this->CF = CF;
-        this->name = name;
-        this->surname = surname;
-    }
+    Supplier(string P_IVA, string email, string pw, vector<int> ID_wh, int ID, string CF, string name, string surname);
 
     /** Prototipo di funzione che aggiunge nel DB un nuovo articolo 
      * 
      * @param a Product da aggiungere
      */
-    void addProduct(Product a);
-    
+    void addProduct(Product a, int q);
+
     /** Prototipo di funzione che permette di marcare un prodotto come discontinuo
      * 
      * @param a Product da marcare come discontinuo
@@ -41,22 +33,7 @@ public:
      * @param p Supplier di cui si vogliono vedere gli articoli venduti
      * @return Vector di articoli venduti
      */
-    vector<Product> deleteSoldProducts(Supplier p);
+    vector<Product> getPastOrders();
 };
-
-/* 
-struct Magazzino {
-    int ID;
-    vector<int> ID_Articoli; // stesso indice in tot indica la quantità
-    vector<int> tot; 
-    string indirizzo;
-    int ID_Supplier; // ID del Supplier proprietario
-}; */
-
-/* TODO magari forse potrebbe essere utile implementare qualche sicurezza */
-
-
-
-
 
 #endif
