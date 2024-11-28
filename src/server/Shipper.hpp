1e8ec1959ef48b74b9e90f4a322e340302cee8f7
romin <rominagrykaa@gmail.com>
2024-11-28 18:51:26 +0100

    Aggiunta e modifica funzioni

--- src/server/Shipper.hpp
+++ src/server/Shipper.hpp
@@ -1,9 +1,32 @@
 #include "utils.hpp"
+#include "User.hpp"
+#include "../dbutils/pgutils.hpp"
 
-struct Trasportatore {
-    int ID;
+class Shipper : public User {
+
+    public:
     string P_IVA;
     string ragione_sociale;
     string sede;            //sarebbe l'indirizzo (da aggiustare)
     vector<Shipment> spedizioni_assegnate;
-};
+
+    Shipper(int ID,string CF, string name, string surname, string email) {
+        this->ID = ID;
+        this->CF = CF;
+        this->name = name;
+        this->surname = surname;
+        this->email = email;
+    }
+
+    std::vector<Shipment> getShippings();
+
+    std::vector<Shipment> getActiveShippings(pqxx::connection& conn);
+
+    static Shipper trasportatore_disponibile(pqxx::connection &conn);
+
+    static void updateShipping(Shipment old_sped, Shipment new_sped);
+
+    static void newShipping(Order o, pqxx::connection &conn);
+
+    static void assignUnassignedOrders(pqxx::connection &conn);
+}
