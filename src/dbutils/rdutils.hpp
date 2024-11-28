e162294c9879eaca27296a4d7280997db683ee66
Sharp-p <thomassharp2003@gmail.com>
2024-11-28 16:27:43 +0100

    get e set in classe RedisCache finite

--- src/dbutils/rdutils.hpp
+++ src/dbutils/rdutils.hpp
@@ -4,16 +4,36 @@
 #include <hiredis/hiredis.h>
 #include <pqxx/pqxx>
 #include "../server/utils.hpp"
+#include "pgutils.hpp"
 
 class RedisCache{
 public:
+    /** @brief Connects to redis. */
     RedisCache(const string& host, int port);
 
+    /** @brief Closes the connection to redis. */
     ~RedisCache();
 
-    bool exist(const string& key);
-    string get(const string& key);
-    void set(const string& key, const string& value);
+    /** @brief Checks the existence of the key inside redis
+     * @param table The table in the DB
+     * @param ID The primary key of the tuple in the DB
+     * @return A boolean, True if the key exists, otherwise False
+     */
+    bool exist(const string& table, const string& ID);
+
+    /** @brief Gets the element from redis, or from the DB if not found
+     * @param table The table in the DB
+     * @param ID The primary key of the tuple in the DB
+     * @return Returns the value of the element as a string
+     */
+    string get(const string& table, const string& ID);
+
+    /** @brief Set an element in the redis cache
+     * @param table The table in the DB
+     * @param ID The primary key of the tuple in the DB
+     * @param value A string of format "value1[,value2][,value3]..." that rapresents the values in the columns of the DB
+    */
+    void set(const string& table, const string& ID, const string& value);
 
 private:
     redisContext* context;
