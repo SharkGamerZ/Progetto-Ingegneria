b764d2deb8dd0398249a596a7dc8624291bec5f7
romin <rominagrykaa@gmail.com>
2024-12-09 18:09:02 +0100

    modifcia shippingDelivered

--- src/server/Shipper.cpp
+++ src/server/Shipper.cpp
@@ -113,7 +113,6 @@ void Shipper::shippingDelivered(int shippingID) {
 }
 
 
-
 /*
 Questa funzione cerca un trasportatore disponibile,che si può ottenere dalla tabella shippers.
 Per "disponibile", si intende un trasportatore che non ha spedizioni attualmente
